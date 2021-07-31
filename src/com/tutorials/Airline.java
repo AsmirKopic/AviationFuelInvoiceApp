121800e6c8a1225542e1453c64b5fb4350fa990f
Asmir Kopic <asmir.kopic123@gmail.com>
2021-07-31 23:40:25 +0200

    Testing object

--- src/com/tutorials/Airline.java
+++ src/com/tutorials/Airline.java
@@ -1,10 +1,5 @@
 package com.tutorials;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Airline {
     public String name;
     private double price;
--- src/com/tutorials/Main.java
+++ src/com/tutorials/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
     private static AvioService avioService = new AvioService();
     public static void main(String[] args) {
+
 	// This program will take input from delivery voucher provided in time of aircraft refueling.
     // Company name, Delivery voucher number, time of refuelling, specific weight of fuel, amount of liters.
     // Quantities will be calculated using specific weight (0.8) on time of refueling.
 
+
+        //testing object
 
         Airline temp = new Airline("Turkish Airlines", 210, 15);
         avioService.addAirline(temp);
@@ -18,9 +21,6 @@ public class Main {
         avioService.addAirline(temp);
 
         avioService.listOfAirlines();
-
-
-
 
     }
 }
