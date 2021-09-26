da763b94c37882ffb7d263194122e166a3fa37ee
AsmirKopic <asmir.kopic123@gmail.com>
2021-09-26 23:13:50 +0200

    AirlineManagement insertNewAirline method added.

--- src/com/tutorials/management/AirlineManagement.java
+++ src/com/tutorials/management/AirlineManagement.java
@@ -3,10 +3,6 @@ package com.tutorials.management;
 import com.tutorials.database.AirlineDaoImpl;
 import com.tutorials.model.Airline;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.lang.reflect.Executable;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,24 +10,25 @@ public class AirlineManagement {
 
     public static final String AIRLINE_MANAGEMENT_MENU =
 
-                    "======= AIRLINE MANAGEMENT MENU ===========\n" +
-                    "===========================================\n" +
-                    "Enter an option\n" +
-                    "===========================================\n" +
-                    "1. List all Airlines in database\n" +
-                    "2. Find Airline by name\n" +
-                    "3. Insert new Airline\n" +
-                    "4. Update Airline\n" +
-                    "5. Delete Airline\n" +
-                    "6. Exit\n" +
-                    "===========================================\n" +
-                    "===========================================";
+            """
+                    ======= AIRLINE MANAGEMENT MENU ===========
+                    ===========================================
+                    Enter an option
+                    ===========================================
+                    1. List all Airlines in database
+                    2. Find Airline by name
+                    3. Insert new Airline
+                    4. Update Airline
+                    5. Delete Airline
+                    6. Exit
+                    ===========================================
+                    ===========================================""";
 
     static AirlineDaoImpl airlineImpl = new AirlineDaoImpl();
 
     public static void runAirlineManagement() {
 
-        int option = 0;
+        int option;
         boolean menu = true;
 
         while (menu) {
@@ -43,34 +40,17 @@ public class AirlineManagement {
                 option = input.nextInt();
 
                 switch (option) {
-                    case 1:
-                        listAllAirlines();
-                        break;
-
-                    case 2:
-                        findAirlineByName();
-                        break;
-
-                    case 3:
-                        insertNewAirline();
-                        break;
-
-                    case 4:
-                        updateAirline();
-                        break;
-
-                    case 5:
-                        deleteAirline();
-                        break;
-
-                    case 6:
+                    case 1 -> listAllAirlines();
+                    case 2 -> findAirlineByName();
+                    case 3 -> insertNewAirline();
+                    case 4 -> updateAirline();
+                    case 5 -> deleteAirline();
+                    case 6 -> {
                         menu = false;
                         System.out.println("Exit Airline management.");
                         System.exit(0);
-
-                    default:
-                        System.out.println("Invalid Option! Please enter again!");
-                        break;
+                    }
+                    default -> System.out.println("Invalid Option! Please enter again!");
                 }
 
             } catch (Exception e) {
@@ -85,10 +65,7 @@ public class AirlineManagement {
         System.out.println("===========================================");
 
         List<Airline> listOfAirlines = airlineImpl.listAllAirlines();
-
-        listOfAirlines.forEach( airline -> {
-            System.out.printf("%-20s $%.2f%n", airline.getName(), airline.getPriceTerms(), airline.getPaymentTerms());
-        });
+        listOfAirlines.forEach( airline -> System.out.printf("%-20s $%.2f%n", airline.getName(), airline.getPriceTerms(), airline.getPaymentTerms()));
 
         System.out.println("===========================================");
 
@@ -97,7 +74,7 @@ public class AirlineManagement {
     public static void findAirlineByName() {
 
         Scanner input = new Scanner(System.in);
-        Airline airline = new Airline();
+        Airline airline;
 
         System.out.println("===========================================");
 
