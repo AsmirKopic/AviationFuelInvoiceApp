89493796ef815c611ec399743eff4dc2ffb18c46
AsmirKopic <asmir.kopic123@gmail.com>
2021-10-13 21:11:21 +0200

    AirlineDaoImplTest class update

--- src/test/DBUtilTest.java
+++ src/test/DBUtilTest.java
@@ -4,6 +4,7 @@ import com.aviationFuelApp.database.DBUtil;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,12 +14,5 @@ class DBUtilTest {
     void getConnectionTest() {
         Connection conn = DBUtil.getConnection();
         assertNotNull(conn, "Connection should be successful");
-    }
-
-    @Test
-    void closeConnection() {
-        Connection conn = DBUtil.getConnection();
-        DBUtil.closeConnection(conn);
-        assertNull(conn, "Connection should be closed");
     }
 }
